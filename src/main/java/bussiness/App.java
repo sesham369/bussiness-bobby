bd0c96a493219c278804d7af7842aeaf70a80662
bob <snm2369@gmail.com>
2022-04-29 09:03:25 +0530

    files of bussiness-bobby

---
+++ src/main/java/bussiness/App.java
@@ -0,0 +1,13 @@
+package bussiness;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
